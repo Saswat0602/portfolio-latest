847fbb87a588a824cc1fb2af3d6a31536042dd66
saswatranjan <saswatranjan0602@gmail.com>
2025-05-17 11:56:44 +0530

    comment out unused CustomCursor import

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React, { lazy, useEffect, useState, Suspense, memo } from 'react';
 import { ThemeProvider } from './context/ThemeContext';
 import Navbar from './components/Navbar';
 import Footer from './sections/Footer';
-import CustomCursor from './components/CustomCursor';
+// import CustomCursor from './components/CustomCursor';
 import LoadingScreen from './components/LoadingScreen';
 // import SplashCursor from './reactbits/SplashCursor';
 
